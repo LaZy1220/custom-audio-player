a37db9f5aeecfecad2849d12f806e9fbc50ede04
LaZy1220 <4elove4iska@gmail.com>
2023-09-22 17:32:15 +0300

    feat: add loading song

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-// Получаем элементы DOM
-const playButton = document.getElementById("playButton");
-const pauseButton = document.getElementById("pauseButton");
-const audio = document.getElementById("myAudio");
+const player = document.querySelector(".player");
+const playBtn = document.querySelector(".play");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const audio = document.querySelector(".audio");
+const progressContainer = document.querySelector(".progress_container");
+const progress = document.querySelector(".progress");
+const title = document.querySelector(".song");
+const cover = document.querySelector(".cover__img");
+const coverText = document.querySelector(".cover__text");
+const imgSrc = document.querySelector(".img__src");
 
-// Обработчики событий для кнопок воспроизведения и паузы
-playButton.addEventListener("click", () => {
+const songs = ["Miss You", "Here We Go Again", "Turn It Up"];
+const authors = [
+  "Oliver Tree & Robin Schulz",
+  "Oliver Tree & David Guetta",
+  "Oliver Tree & Little Big & Tommy Cash",
+];
+const songIndex = 0;
+
+coverText.innerHTML = authors[songIndex];
+function loadSong(song) {
+  console.log(song);
+  title.innerHTML = song;
+  audio.src = `audio/${song}.mp3`;
+}
+loadSong(songs[songIndex]);
+
+function playSong() {
+  player.classList.add("play");
   audio.play();
-});
-
-pauseButton.addEventListener("click", () => {
-  audio.pause();
+}
+function pauseSong() {
+  audio.stop();
+}
+playBtn.addEventListener("click", () => {
+  playSong();
 });
