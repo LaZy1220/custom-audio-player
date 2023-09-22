8e539c847c2f9888a3265b1e4428c357187df82d
LaZy1220 <4elove4iska@gmail.com>
2023-09-22 20:13:39 +0300

    feat: add progress bar

--- script.js
+++ script.js
@@ -5,34 +5,70 @@ const nextBtn = document.querySelector(".next");
 const audio = document.querySelector(".audio");
 const progressContainer = document.querySelector(".progress_container");
 const progress = document.querySelector(".progress");
-const title = document.querySelector(".song");
-const cover = document.querySelector(".cover__img");
+const author = document.querySelector(".author");
+const cover = document.querySelector(".cover");
 const coverText = document.querySelector(".cover__text");
 const imgSrc = document.querySelector(".img__src");
 
-const songs = ["Miss You", "Here We Go Again", "Turn It Up"];
+const songs = ["Turn It Up", "Here We Go Again", "Miss You"];
 const authors = [
-  "Oliver Tree & Robin Schulz",
-  "Oliver Tree & David Guetta",
   "Oliver Tree & Little Big & Tommy Cash",
+  "Oliver Tree & David Guetta",
+  "Oliver Tree & Robin Schulz",
 ];
-const songIndex = 0;
+let songIndex = 0;
 
-coverText.innerHTML = authors[songIndex];
-function loadSong(song) {
-  console.log(song);
-  title.innerHTML = song;
+function loadSong(song, authorText) {
+  author.innerHTML = authorText;
+  coverText.innerHTML = song;
   audio.src = `audio/${song}.mp3`;
 }
-loadSong(songs[songIndex]);
+loadSong(songs[songIndex], authors[songIndex]);
 
 function playSong() {
   player.classList.add("play");
+  cover.classList.add("active");
+  imgSrc.src = "./img/pause.png";
   audio.play();
 }
 function pauseSong() {
-  audio.stop();
+  player.classList.remove("play");
+  cover.classList.remove("active");
+  imgSrc.src = "./img/play.png";
+  audio.pause();
 }
 playBtn.addEventListener("click", () => {
-  playSong();
+  const isPlaying = player.classList.contains("play");
+  if (!isPlaying) {
+    playSong();
+  } else {
+    pauseSong();
+  }
 });
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex], authors[songIndex]);
+  playSong();
+}
+nextBtn.addEventListener("click", nextSong);
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex], authors[songIndex]);
+  playSong();
+}
+prevBtn.addEventListener("click", prevSong);
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPersent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPersent}%`;
+}
+audio.addEventListener("timeupdate", updateProgress);
